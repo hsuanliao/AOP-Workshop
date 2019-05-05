bf037cb999452112e238a880b95ee97422d83fc6
Kevin Liao <kevin.liao@mail.aircast.biz>
2019-05-05 21:50:21 +0800

    Implement console Main

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -1,9 +1,51 @@
-﻿namespace MyConsole
+﻿using System;
+using MyWalletLib;
+using MyWalletLib.Models;
+
+namespace MyConsole
 {
+    internal class FakeBankingAdapter : IBanking
+    {
+        public void Deposit(string bankingAccount, decimal amount)
+        {
+            Console.WriteLine($"{nameof(Deposit)}({bankingAccount}, {amount})");
+        }
+
+        public void Withdraw(string bankingAccount, decimal amount)
+        {
+            Console.WriteLine($"{nameof(Withdraw)}({bankingAccount}, {amount})");
+        }
+    }
+
+    internal class FakeFeeAdapter : IFee
+    {
+        public decimal GetFee(string account)
+        {
+            Console.WriteLine($"{nameof(GetFee)}({account})");
+            return 10;
+        }
+    }
+
+    internal class FakeWalletRepo : IWalletRepo
+    {
+        public void UpdateDelta(string account, decimal amount)
+        {
+            Console.WriteLine($"{nameof(UpdateDelta)}({account}, {amount})");
+        }
+    }
+
     internal class Program
     {
         private static void Main(string[] args)
         {
+            var fakeWalletRepo = new FakeWalletRepo();
+            var fakeBankingAdapter = new FakeBankingAdapter();
+            var fakeFeeAdapter = new FakeFeeAdapter();
+
+            var wallet = new Wallet(fakeWalletRepo, fakeBankingAdapter, fakeFeeAdapter);
+
+            wallet.Deposit("joey", 1000, "123456789");
+            wallet.Withdraw("joey", 1000, "123456789");
         }
     }
 }
